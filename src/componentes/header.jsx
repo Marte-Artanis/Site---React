de776d215c366bb9f8aa8f51f6b1f4d10401f646
DiegoCheib <diegocheib@gmail.com>
2024-06-08 20:16:06 -0300

    Alteração 1

---
+++ src/componentes/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return (
+
+    <h1>
+        Contato:@
+    </h1>
+
+    )
+}
+
+export default Footer
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./componentes/header";
+import Footer from "./componentes/footer";
+
+function Main(){
+    return(
+        <div>
+
+        <Header/>
+
+        <h1>
+            Olá!
+        </h1>
+
+        <Footer/>
+
+        </div>
+        // tudo que se escreve aqui dentro é html
+    )
+}
+
+export default Main
